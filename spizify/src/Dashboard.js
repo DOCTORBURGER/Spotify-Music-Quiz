efab77e2b1999a61a1a474fed062b1fa33c30ca3
ashsh <alexheinking71@gmail.com>
2022-11-12 14:12:42 -0600

    dashboard load

--- spizify/src/Dashboard.js
+++ spizify/src/Dashboard.js
@@ -2,6 +2,12 @@ import React from 'react'
 import useAuth from './useAuth'
 
 export default function Dashboard({ code }) {
-    const accessToken = useAuth(code)
-    return <div>{code}</div>
+    //const accessToken = useAuth(code)
+    return (<div>
+        <nav className='Header py-2 text-black mb-4' style={{ backgroundColor: 'var(--accent-color)' }}>
+            <div className='container text-center'>
+                <h3 className='py-3 mx-2'>Todo App: React & Bootstrap</h3>
+            </div>
+        </nav>
+    </div>)
 }
