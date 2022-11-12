f38f536ccf5ad3c6617203eaf79812d5ed6731ec
Jonah Bogusch <jonahmbog@gmail.com>
2022-11-12 17:53:15 -0600

    Added autoplay

--- spizify/src/Player.js
+++ spizify/src/Player.js
@@ -1,10 +1,18 @@
-import React from "react"
+import { useState, useEffect } from "react"
 import SpotifyPlayer from "react-spotify-web-playback"
 
 export default function Player({accessToken, trackUri}) {
+    const [play, setPlay] = useState(false)
+
+    useEffect(() => setPlay(true), [trackUri])
+
     if (!accessToken) return null
     return <SpotifyPlayer 
         token={accessToken}
+        callback={state => {
+            if(!state.isPlaying) setPlay(false)
+        }}
+        play = {play}
         uris={trackUri ? [trackUri] : []}
     />
 }
