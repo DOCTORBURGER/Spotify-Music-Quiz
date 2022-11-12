9acd2e3315383f999a2a11647fe87624985f25e4
Jonah Bogusch <jonahmbog@gmail.com>
2022-11-12 14:50:09 -0600

    Refresh working

--- server/server.js
+++ server/server.js
@@ -7,6 +7,27 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json())
 
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: '7be5175ad2c840fd802af2ec7a0d0b92',
+        clientSecret: '0e8c6bab5263414695c01393e2e7ddfd',
+        refreshToken
+    })
+
+    spotifyApi.refreshAccessToken().then(
+        (data) => {
+          res.json({
+            accessToken: data.body.accessToken,
+            expiresIn: data.body.expiresIn
+          })
+        }).catch((err) => {
+            console.log(err)
+            res.sendStatus(400)
+        })
+})
+
 app.post('/login', (req, res) => {
     const code = req.body.code
     const spotifyApi = new SpotifyWebApi({
