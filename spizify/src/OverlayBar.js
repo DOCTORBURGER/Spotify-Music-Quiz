be40a47ccd3f714ade3279538916bee27350fbb5
jrmckown <jrmckown@ksu.edu>
2022-11-12 16:33:24 -0600

    added pretty color

--- spizify/src/OverlayBar.js
+++ spizify/src/OverlayBar.js
@@ -4,7 +4,7 @@ import './styles.css';
 export default function OverlayBar() {
     
     return (
-        <Navbar bg="dark" expand="lg">
+        <Navbar className='banner' expand="lg">
             <span className='span'>
                 <div className='left green'>Spizify</div>
                 <div className='right green'>Logged in as: </div>
