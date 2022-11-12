2058de0a340eb2975292654fd8ede6462b3993e9
DOCTORBURGER <jonahmbog@gmail.com>
2022-11-12 02:31:12 -0600

    Trouble with auth, finish tomorrow

--- spizify/src/Login.js
+++ spizify/src/Login.js
@@ -5,8 +5,13 @@ const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=7be5175ad2c84
 
 export default function Login() {
     return (
-        <Container className="d-flex justify-content-center align-items-center" style={{minHeight: "100vh" }}>
-            <a className="btn btn-success btn-lg" href={AUTH_URL}>Login With Spotify</a>
+        <Container 
+            className="d-flex justify-content-center align-items-center" 
+            style={{minHeight: "100vh" }}
+        >
+            <a className="btn btn-success btn-lg" href={AUTH_URL}>
+                Login With Spotify
+            </a>
         </Container>
     )
 }
